0018a1a50fba5b02fe52d8bf62b88eccb556c4d0
root <root@cyborg.cyborg.com.br>
2018-01-13 14:32:06 -0200

    first commit

---
+++ alurapic/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1 >{{titulo}}</h1>
+    <ul>
+      <li v-for="foto of fotos">
+        <img :src="foto.url" :alt="foto.titulo">
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      titulo: 'Alurapic', 
+      fotos: [
+        {
+        url: 'https://www.anda.jor.br/wp-content/uploads/2016/12/entenda-o-que-significam-os-sons-e-linguagens-corporais-dos-cachorros.jpg',
+        titulo: 'cachorro'
+        },
+        {
+          url: 'https://www.anda.jor.br/wp-content/uploads/2016/12/entenda-o-que-significam-os-sons-e-linguagens-corporais-dos-cachorros.jpg',
+          titulo: 'cachorrao'
+        }
+      ]
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
