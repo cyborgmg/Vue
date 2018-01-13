0dad3487bd95ccef34df61286591bec187f3fb00
Unknown <cyborgmg@gmail.com>
2018-01-13 17:00:11 -0200

    aula 5-4

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,36 +1,90 @@
 <template>
-  <div>
-    <h1 >{{titulo}}</h1>
-    <ul>
-      <li v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.titulo">
+
+  <div class="corpo">
+
+    <h1 class="centralizado" >{{titulo}}</h1>
+
+    <input type="search" class="filtro" placeholder="Filtre por parte do titulo" v-on:input="filtro = $event.target.value" >
+    {{ filtro }}
+    <ul class="lista-fotos">
+
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
+
+        <meu-painel :titulo="foto.titulo" >
+            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
+
       </li>
+
     </ul>
 
   </div>
+
 </template>
 
 <script>
+import Painel from './componentes/shared/painel/Painel.vue';
+
 export default {
+
+  components: {
+    'meu-painel' : Painel
+  },
+  
   data(){
     return{
       titulo: 'Alurapic', 
-      fotos: [
-        {
-        url: 'https://www.anda.jor.br/wp-content/uploads/2016/12/entenda-o-que-significam-os-sons-e-linguagens-corporais-dos-cachorros.jpg',
-        titulo: 'cachorro'
-        },
-        {
-          url: 'https://www.anda.jor.br/wp-content/uploads/2016/12/entenda-o-que-significam-os-sons-e-linguagens-corporais-dos-cachorros.jpg',
-          titulo: 'cachorrao'
-        }
-      ]
-      
+      fotos: [],
+      filtro: ''
+    }
+  },
+
+  created(){
+    this.$http.get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos, err => console.log(err) );
+  },
+
+  computed: {
+    fotosComFiltro(){
+      if(this.filtro){
+        let exp = new RegExp(this.filtro.trim(),'i');
+        return this.fotos.filter(foto => exp.test(foto.titulo));
+      }else{
+        return this.fotos;
+      }
     }
   }
+
 }
 </script>
 
 <style>
+.centralizado {
+    text-align: center;
+  }
+
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .imagem-responsiva {
+    width: 100%;
+  }
+
+  .filtro {
+    display: block;
+    width: 100%;
+  }
 
 </style>
